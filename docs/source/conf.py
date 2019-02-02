cc22818acf8e351a9a36fbb938d6a0825fd0edfa
Markus Englund <jmenglund@users.noreply.github.com>
2019-02-02 15:52:54 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,7 +69,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pandas-validation'
-copyright = '2016, Markus Englund'
+copyright = '2016–2019, Markus Englund'
 author = 'Markus Englund'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -168,7 +168,7 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
